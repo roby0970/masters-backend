cb7f898e7e8edf09554201c8c75dfe0d1a8f320b
Robert <roby0970@hotmail.com>
2021-08-25 01:04:59 +0200

    trying websockets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, post, web};
+
+use actix_web::{middleware, App, HttpRequest, HttpResponse, HttpServer, Responder, get, post, web, Error};
 use actix_cors::Cors;
+use actix::prelude::*;
+use actix_web_actors::ws;
 use serde::{Serialize, Deserialize};
+
+
+
+
 #[macro_use]
 extern crate diesel;
 #[macro_use]
@@ -18,11 +25,42 @@ async fn welcome(request: HttpRequest) -> impl Responder {
     let name = request.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
+
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(&text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+
+async fn websocket(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
          .wrap(Cors::permissive())
+         .route("/ws/", web::get().to(websocket))
             .route("/welcome", web::get().to(welcome))
             .route("/welcome/{name}", web::get().to(welcome))
             .configure(spaces::init_routes)
